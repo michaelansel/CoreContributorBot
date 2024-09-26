8483b076257e5186e840deaa48fb3bb6d532d365
Michael Ansel <git@anselcomputers.com>
2024-09-26 19:43:56 +0000

    Wow the GH API is confusing

--- bot.py
+++ bot.py
@@ -344,9 +344,12 @@ if __name__ == '__main__':
                 process_issue(issue)
 
         for pr in repo.get_pulls():
-            comments = list(pr.get_comments())
-            if comments:
-                most_recent_comment = sorted(comments, key=lambda comment: comment.created_at)[-1]
-                if not most_recent_comment.body.startswith("Bot Response: "):
-                    log("Handling a pull request")
-                    process_pull_request_comment(pr, most_recent_comment)
+            log(f"PR: {pr.title} ({pr.state}): {pr.comments}")
+            if pr.state == 'open' and pr.comments > 0:
+                comments = list(pr.get_issue_comments())
+                if comments:
+                    most_recent_comment = sorted(comments, key=lambda comment: comment.created_at)[-1]
+                    log(f'Most recent comment: {most_recent_comment}')
+                    if not most_recent_comment.body.startswith("Bot Response: "):
+                        log("Handling a pull request")
+                        process_pull_request_comment(pr, most_recent_comment)
