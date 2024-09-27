f02aeda0fd3e237c20f54173957b60942b9feedd
Michael Ansel <git@anselcomputers.com>
2024-09-27 01:35:24 +0000

    Update how tests are launched

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 source .env
-docker run -it --rm -e "GITHUB_TOKEN=$GITHUB_TOKEN" -e "OPENAI_API_KEY=$OPENAI_API_KEY" -e "GH_REPO=$GH_REPO" github-bot python bot.py test
+docker run -it --rm -e "GITHUB_TOKEN=$GITHUB_TOKEN" -e "OPENAI_API_KEY=$OPENAI_API_KEY" -e "GH_REPO=$GH_REPO" github-bot python -m unittest
