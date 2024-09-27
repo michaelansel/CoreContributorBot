308c1809fafc4a2c96d96da43184b7abd772270b
Michael Ansel <github@anselcomputers.com>
2024-09-26 22:08:08 -0400

    Update lib/github.py to address issue #1

--- lib/github.py
+++ lib/github.py
@@ -1,10 +1,7 @@
 from github import Github
-import os
 
-# Initialize GitHub API client
-gh_token = os.environ['GITHUB_TOKEN']
-gh = Github(gh_token)
-
-# Define the repository to monitor
-repo_name = os.environ['GH_REPO']
-repo = gh.get_repo(repo_name)
+def repo():
+    token = os.environ.get('GITHUB_TOKEN')
+    repo_name = os.environ.get('GH_REPO')
+    g = Github(token)
+    return g.get_repo(repo_name)
