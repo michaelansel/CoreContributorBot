ee04385fd07faf29de9eaaef5469c32305c19f90
Michael Ansel <github@anselcomputers.com>
2024-09-27 16:51:46 -0400

    Update lib/constants.py to address issue #24

--- lib/constants.py
+++ lib/constants.py
@@ -1,3 +1,1 @@
-# Special keyword that can never appear in the code or it breaks the self-management behavior
-# Reason: we split files in the LLM output on this keyword, so it should only exist in the LLM "meta" output
-SPECIAL_BEGIN_FILE_CONTENTS_DELIMETER = " ".join(["###","BEGIN","FILE","CONTENTS"])
+SPECIAL_BEGIN_FILE_CONTENTS_DELIMETER = "### BEGIN FILE CONTENTS: "
