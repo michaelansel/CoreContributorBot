28bb6008510d3c35352136e7f3e515edb11b022c
Michael Ansel <git@anselcomputers.com>
2024-09-30 18:33:52 +0000

    Fix missing argument and other edge cases

--- lib/commit_files.py
+++ lib/commit_files.py
@@ -5,13 +5,24 @@ def commit_files(branch, files, reason=" just because"):
         # Update the files with the generated code changes
         for filename, content in files.items():
             if content.strip() == "":
-                # Delete the file if the content is empty
-                log(f"Deleting empty file: {filename}")
-                repo.delete_file(
-                    path=filename,
-                    message=f"Delete {filename}{reason}",
-                    branch=branch
-                )
+                # Delete the file if the content is empty, but only if it previously existed
+                try:
+                    file = repo.get_contents(filename, ref=branch)
+                    log(f"Deleting empty file: {filename}")
+                    repo.delete_file(
+                        path=filename,
+                        message=f"Delete {filename}{reason}",
+                        branch=branch,
+                        sha=file.sha,
+                    )
+                except:
+                     log(f"Creating empty file: {filename}")
+                     repo.create_file(
+                        path=f'{filename}',
+                        message=f'Create {filename}{reason}',
+                        content=content,
+                        branch=branch,
+                    )
             else:
                 try:
                     file = repo.get_contents(filename, ref=branch)
@@ -27,7 +38,7 @@ def commit_files(branch, files, reason=" just because"):
                     log(f"Creating file: {filename}")
                     repo.create_file(
                         path=f'{filename}',
-                        message=f'Update {filename}{reason}',
+                        message=f'Create {filename}{reason}',
                         content=content,
                         branch=branch,
                     )
