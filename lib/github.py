b6e6312661bdfb42273df913e128493c6d6bb2af
Michael Ansel <github@anselcomputers.com>
2024-09-30 18:58:17 -0400

    Update lib/github.py based on feedback

--- lib/github.py
+++ lib/github.py
@@ -1,10 +1,9 @@
+# Initialize GitHub API client and define the repository to monitor
 from github import Github
 import os
 
-# Initialize GitHub API client
 gh_token = os.environ['GITHUB_TOKEN']
 gh = Github(gh_token)
 
-# Define the repository to monitor
 repo_name = os.environ['GH_REPO']
 repo = gh.get_repo(repo_name)
