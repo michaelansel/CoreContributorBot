52b5031c69333c925eaf6b499c53757b3fba9287
Michael Ansel <github@anselcomputers.com>
2024-09-30 08:56:10 -0400

    Update lib/parse_code_changes.py based on feedback

--- lib/parse_code_changes.py
+++ lib/parse_code_changes.py
@@ -1,12 +1,10 @@
-from .constants import SPECIAL_BEGIN_FILE_CONTENTS_DELIMETER
-from .log import log
-
 def parse_code_changes(code_changes):
     """
     Parse the code changes from the LLM output.
     """
     files = code_changes.split(SPECIAL_BEGIN_FILE_CONTENTS_DELIMETER)
     changes_dict = {}
+    
     for file in files:
         file = file.strip()
         if file:
