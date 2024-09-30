8da3c66ee383f681a7b3e25590e7b7049bbe4734
Michael Ansel <github@anselcomputers.com>
2024-09-30 18:58:23 -0400

    Update lib/pr_helpers.py based on feedback

--- lib/pr_helpers.py
+++ lib/pr_helpers.py
@@ -1,9 +1,7 @@
+# Helper functions for processing pull requests
 from .github import repo
 
 def extract_issue_from_pull_request(pr_body):
-    """
-    Extract the original issue title and description from the pull request body.
-    """
     if pr_body.startswith("Address issue #"):
         number = pr_body.split("Address issue #", 1)[1]
         issue = repo.get_issue(number=int(number))
@@ -12,9 +10,6 @@ def extract_issue_from_pull_request(pr_body):
     raise Exception("can't find the associated issue")
 
 def get_proposed_changes(pr):
-    """
-    Get the proposed code changes from the pull request.
-    """
     proposed_changes = {}
     for file in pr.get_files():
         proposed_changes[file.filename] = file.patch
